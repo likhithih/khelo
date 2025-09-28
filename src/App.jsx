bcfef71b1e90f3fc444a13963601c504c192bf41
likhithih <likhithih2003@gmail.com>
2025-09-28 20:03:42 +0530

    navbar creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Navbar from './component/Navbar';
+
 function App() {
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-  <Route path="/" element={<Login/>} />
-  <Route path="/register" element={<Register/>} />
-</Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+
+        <Navbar />  
+        
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function Navbar() {
+//   const navStyle = {
+//     backgroundColor: "red",
+//     borderRadius: "20px",
+//     width: "6%",
+//     height: "30px",
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   };
+
+//   const linkStyle = {
+//     color: "white",
+//     textDecoration: "none",
+//     fontWeight: "bold",
+//   };
+
+//   return (
+//     <div
+//       className="navbar"
+//       style={{
+//         display: "flex",
+//         justifyContent: "space-evenly",
+//         backgroundColor: "black",
+//         color: "white",
+//         height: "60px",
+//         alignItems: "center",
+//       }}
+//     >
+//       <div style={navStyle}><Link to="/" style={linkStyle}>Home</Link></div>
+//       <div style={navStyle}><Link to="/about" style={linkStyle}>About</Link></div>
+//       <div style={navStyle}><Link to="/contact" style={linkStyle}>Contact</Link></div>
+//       <div style={navStyle}><Link to="/login" style={linkStyle}>Login</Link></div>
+//       <div style={navStyle}><Link to="/register" style={linkStyle}>Register</Link></div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
